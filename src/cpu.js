3e98f407e193b50a069537e23cebbc34da2bd401
Kia Choi <sgchoi529@email.arizona.edu>
2018-01-26 14:07:49 -0700

    Completed IRET and PRA

--- src/cpu.js
+++ src/cpu.js
@@ -7,18 +7,19 @@ import { setInterval, clearInterval } from 'timers';
 const fs = require('fs');
 
 // Instructions
-const HLT  = 0b00011011; // Halt CPU
-// LDI
+const HLT = 0b00011011; // Halt CPU
 const LDI = 0b00000100; // Load Register Immediate
-// MUL
 const MUL = 0b00000101; // Multiply Register Register
-// PRN
 const PRN = 0b00000110; // Print (also 6 in binary)
 const PUSH = 0b00001010; // Push
 const POP = 0b00001011; // Pop
 const CALL = 0b00001111; // Call
 const RET = 0b00010000; // Return
+const ADD = 0b00001100; // Add R R
 const JMP = 0b00010001; // Jump
+const ST = 0b00001001; // Store R
+const IRET = 0b00011010; // Return from interrupt
+const PRA = 0b00000111; // Print alpha
 
 const IS = 6;
 const IM = 5;
@@ -35,41 +36,42 @@ class CPU {
         this.ram = ram;
 
         this.reg = new Array(8).fill(0); // General-purpose registers -- registers R0 - R7
-        
+
         this.reg[7] = 0xF8; // Initializing SP
 
-            // Special-purpose registers
-            this.reg.PC = 0; // Program Counter
+        // Special-purpose registers
+        this.reg.PC = 0; // Program Counter
         this.reg.IR = 0; // Instruction Register
 
         this.flags = {
             interruptsEnabled: true
         };
 
-		this.setupBranchTable();
+        this.setupBranchTable();
     }
-	
+
 	/**
 	 * Sets up the branch table
 	 */
-	setupBranchTable() {
-		let bt = {};
+    setupBranchTable() {
+        let bt = {};
 
         bt[HLT] = this.HLT;
-        // LDI
         bt[LDI] = this.LDI;
-        // MUL
         bt[MUL] = this.MUL;
-        // PRN
         bt[PRN] = this.PRN;
         bt[PUSH] = this.PUSH;
         bt[POP] = this.POP;
         bt[CALL] = this.CALL;
         bt[RET] = this.RET;
         bt[JMP] = this.JMP;
+        bt[ADD] = this.ADD;
+        bt[ST] = this.ST;
+        bt[IRET] = this.IRET;
+        bt[PRA] = this.PRA;
 
-		this.branchTable = bt;
-	}
+        this.branchTable = bt;
+    }
 
     /**
      * Store value in memory address, useful for program loading
@@ -136,7 +138,7 @@ class CPU {
                 if (((maskedInterrupts >> i) & 1) === 1) {
                     // Disabling interrupts
                     this.flags.interruptsEnabled = false;
-                    
+
                     // Clearing the ith bit in the IS
                     this.reg[IS] &= ~(1 << i);
 
@@ -156,7 +158,7 @@ class CPU {
 
                     // Set PC to handler address
                     this.reg.PC = handlerAddress;
-                    
+
                     // console.log(`handling interrupted! ${i}`);
                     break;
                 }
@@ -183,7 +185,7 @@ class CPU {
             this.stopClock(); // stops the interval OR can call HLT()
             return; // because we don't want to call the handler below
         }
-        
+
         // We need to use call() so we can set the "this" value inside
         // the handler (otherwise it will be undefined in the handler)
         handler.call(this);
@@ -231,24 +233,7 @@ class CPU {
         const regA = this.ram.read(this.reg.PC + 1);
         console.log(this.reg[regA]);
         this.reg.PC += 2;
-    }
 
-    ADD() {
-        const regA = this.ram.read(this.reg.PC + 1);
-        const regB = this.ram.read(this.reg.PC + 2);
-
-        this.alu('ADD', regA, regB);
-
-        this.reg.PC += 3;
-    }
-
-    ST() {
-        const regA = this.ram.read(this.reg.PC + 1);
-        const regB = this.ram.read(this.read.PC + 2);
-
-        this.ram.write(this.reg[regA], this.reg[regB]);
-
-        this.reg.PC += 3;
     }
 
     /**
@@ -277,7 +262,6 @@ class CPU {
 
         this.reg.PC += 2;
     }
-
     /**
      * CALL R
      */
@@ -303,8 +287,56 @@ class CPU {
      */
     JMP() {
         const regA = this.ram.read(this.reg.PC + 1);
-        
+
         this.reg.PC = this.reg[regA];
+    }
+
+    /**
+     * ADD R R
+     */
+    ADD() {
+        const regA = this.ram.read(this.reg.PC + 1);
+        const regB = this.ram.read(this.reg.PC + 2);
+
+        this.alu('ADD', regA, regB);
+
+        this.reg.PC += 3;
+    }
+
+    /**
+     * ST R R
+     */
+    ST() {
+        const regA = this.ram.read(this.reg.PC + 1);
+        const regB = this.ram.read(this.read.PC + 2);
+
+        this.ram.write(this.reg[regA], this.reg[regB]);
+
+        this.reg.PC += 3;
+    }
+
+    /**
+     * IRET 
+     */
+    IRET() {
+        for (let i = 7; i >= 0; i--) {
+            this.reg[i] = this.ram.read(this.reg[7]);
+            this.reg[7]++;
+        }
+
+        this.reg.PC = this.ram.read(this.reg[7]);
+        this.reg[7]++;
+
+        this.flags.interruptsEnabled = true;
+    }
+
+    /**
+     * PRINT alpha
+     */
+    PRA() {
+        const regA = this.ram.read(this.reg.PC + 1);
+        console.log(String.fromCharCode(this.reg[regA]));
+        this.reg.PC += 2;
     }
 }
 
